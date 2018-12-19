e986c063aab210d6a2433f5224eec1f013c04e2b
Juho Kesälä <elukka.elukka@gmail.com>
2018-12-19 13:26:58 +0200

    Add card base and fonts

--- pillow-experiments/card_drawer.py
+++ pillow-experiments/card_drawer.py
@@ -1,11 +1,13 @@
-from PIL import Image, ImageDraw
+from PIL import Image, ImageDraw, ImageFont
 
 BIG_CARD_RES = (1269, 2205)
 MED_CARD_RES = (508, 883)
 
 def draw():
-    image = Image.open("images/test.png")
-    d = ImageDraw.Draw(image)
-    d.text((10,10), "Hello World", fill=(255,255,0))
+    image = Image.open("assets/templates/card_base_1.png")
+    font = ImageFont.truetype("assets/fonts/Oswald-Bold.ttf", 95)
 
-    image.save("images/result.png")
+    d = ImageDraw.Draw(image)
+    d.text((10,10), "Hello World", fill=(255,255,0), font=font)
+
+    image.save("assets/result.png")
